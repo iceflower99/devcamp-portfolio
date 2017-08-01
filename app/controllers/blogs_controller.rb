f7fa3fe8ac50e7a0ca5387036ab2b6f934255964
Jordan Hudgens <jordan@devcamp.com>
2017-08-01 15:22:01 +0000

    Added validation toppics in blogs and added toppic form elment

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,7 +5,7 @@ class BlogsController < ApplicationController
 
   # GET /blogs
   # GET /blogs.json
-   def index
+  def index
     if logged_in?(:site_admin)
       @blogs = Blog.recent.page(params[:page]).per(5)
     else
@@ -14,10 +14,9 @@ class BlogsController < ApplicationController
     @page_title = "My Portfolio Blog"
   end
 
-
   # GET /blogs/1
   # GET /blogs/1.json
- def show
+  def show
     if logged_in?(:site_admin) || @blog.published?
       @blog = Blog.includes(:comments).friendly.find(params[:id])
       @comment = Comment.new
@@ -92,6 +91,6 @@ class BlogsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
-      params.require(:blog).permit(:title, :body)
+      params.require(:blog).permit(:title, :body, :topic_id)
     end
 end
